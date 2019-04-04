9375ca20a5b98f7481df0658c117dc7830407a80
Sagar Shah <sagarshah2326@gmail.com>
2019-04-04 22:48:02 +0200

    build: soong: Refractor for cosmic
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -20,7 +20,7 @@ import (
 	"runtime"
 	"strings"
 
-	"gzosp/soong/android"
+	"cosmic/soong/android"
 
 	"github.com/google/blueprint/proptools"
 )
@@ -117,8 +117,8 @@ type variableProperties struct {
 			Srcs         []string
 		}
 
-		// include Gzosp variables
-		Gzosp android.Product_variables
+		// include cosmic variables
+		Cosmic android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -237,8 +237,8 @@ type productVariables struct {
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
 
-	// include Gzosp variables
-	Gzosp android.ProductVariables
+	// include cosmic variables
+	Cosmic android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
